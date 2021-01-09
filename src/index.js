fc1bc36b00ee68c5d2db23af8a6685c1dc43e378
Liz Verde <liz@goramandvincent.com>
2021-01-09 16:27:22 +0000

    Adding in a parent component and using props.children

--- src/index.js
+++ src/index.js
@@ -1,30 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import CommentDetail from './CommentDetail';
 import faker from 'faker';
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 
 const App = () => {
     return (
     <div className="ui container comments">
-        <CommentDetail 
-            author="Helen" 
-            timeAgo="Today at 4:15PM" 
-            comment="Well done!" 
-            avatar={faker.image.image()}
-        />
-        <CommentDetail 
-            author="Jon" 
-            timeAgo="Today at 2PM" 
-            comment="Great stuff!" 
-            avatar={faker.image.image()}
-        />
+        <ApprovalCard>
+            <div>
+                <h4>Warning!</h4>
+                <p>Are you sure you want to do this?</p>
+            </div>
+        </ApprovalCard>
+        <ApprovalCard> 
+            <CommentDetail 
+                author="Helen" 
+                timeAgo="Today at 4:15PM" 
+                comment="Well done!" 
+                avatar={faker.image.image()}
+            />
+        </ApprovalCard>
+        <ApprovalCard> 
+            <CommentDetail 
+                author="Jon" 
+                timeAgo="Today at 2PM" 
+                comment="Great stuff!" 
+                avatar={faker.image.image()}
+            />
+        </ApprovalCard>
+        <ApprovalCard>
         <CommentDetail 
             author="Liz" 
             timeAgo="Yesterday at 5PM" 
             comment="This is wicked!" 
             avatar={faker.image.image()}
         />
+        </ApprovalCard>
     </div>
     );
 };
